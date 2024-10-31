703b00ac4b6ec5bc1cfdfcf7b7e6ea6ee0678dfa
Jose Alexander Bermudez <jabermudez99@misena.edu.co>
2024-10-31 17:28:59 -0500

    Practicando modulos

--- hello_youtube.py
+++ hello_youtube.py
@@ -1,2 +1,27 @@
 #module = a file containing python code. May contain functions, classes, etc.
 #used with modular programming, which is to separate a program into parts
+'''
+import messages 
+import messages as m
+
+m.hello()
+m.bye()
+from messages import hello, bye
+from messages import * (this one is dangerous)
+'''
+#help("modules")
+
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+computer = random.choice(choices)
+player = None
+
+while player not in choices:
+    player = input("rock, paper or scissors?: ").lower()
+
+if player == computer: 
+    print("computer: ", computer)
+    print("player: ", player)
+    print("Tie!")
