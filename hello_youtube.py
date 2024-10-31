d085f09eca6c0b0a7a19ee3d13c7fb9de9665e83
Jose Alexander Bermudez <jabermudez99@misena.edu.co>
2024-10-31 17:48:09 -0500

    rock, paper and scissors

--- hello_youtube.py
+++ hello_youtube.py
@@ -10,18 +10,3 @@ from messages import hello, bye
 from messages import * (this one is dangerous)
 '''
 #help("modules")
-
-import random
-
-choices = ["rock", "paper", "scissors"]
-
-computer = random.choice(choices)
-player = None
-
-while player not in choices:
-    player = input("rock, paper or scissors?: ").lower()
-
-if player == computer: 
-    print("computer: ", computer)
-    print("player: ", player)
-    print("Tie!")
